27ff0d01cc22726de11368ebc78bbbd512173c66
panggpb <Intita.pang@gmail.com>
2025-05-10 23:54:31 +0700

    update DSI321 project

--- work/deploy.py
+++ work/deploy.py
@@ -1,18 +1,16 @@
 from prefect import flow
 from pathlib import Path
 
-source=str(Path.cwd())
+source = str(Path.cwd())
 entrypoint = f"pipeline.py:main_flow"
 print(f'entrypoint:{entrypoint}, source:{source}')
-
 
 if __name__ == "__main__":
     flow.from_source(
         source=source,
         entrypoint=entrypoint,
     ).deploy(
-        name="airquality_deployment",
-        parameters={},
+        name="air4thai_pipeline_deployment",
         work_pool_name="default-agent-pool",
-        cron="40 * * * *",
+        cron="40 * * * *", # Runs at the start of the hour (minute 40).
     )
--- work/pipeline.py
+++ work/pipeline.py
@@ -1,78 +1,66 @@
 import requests
 import pandas as pd
-from prefect import flow, task
-#try
+from prefect import flow, task, get_run_logger
+
 @task
-def get_aqi_data() -> list[dict]:
-    """
-    make an API request to the endpoint using URL.
-
-    Parameters:
-    None
-
-    Return:
-    data: extracted data for further processing.
-    """
-
-    # API endpoint
-    AQI_ENDPOINT ='http://air4thai.pcd.go.th/services/getNewAQI_JSON.php'
-
-    try:
-        #make API request
-        response = requests.get(url=AQI_ENDPOINT)
+def retrieve_from_api(
+    base_url: str,
+    path: str
+):
+    try:    
+        logger = get_run_logger()
+        response = requests.get(url=base_url + path)
         response.raise_for_status()
+        AQI_stats = response.json()
+        data = AQI_stats['stations']
+        #logger.info(AQI_stats)
+        num_records = len(data)
+        num_columns = len(next(iter(data), {}))
 
-        #transfrom data into data frame
-        response_json = response.json()
-        data = response_json['stations']
+        logger.info(f"Data contains {num_records} records and {num_columns} columns.")
+        logger.info(f"Successfully fetched data from {base_url + path}")
 
         return data
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        return None
     
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching data: {e}")
+        return None
     except KeyError as e:
-        print(f"Error processing data: Missing key {e}")
+        logger.error(f"Error processing data: Missing key {e}")
         return None
 
 
 @task
-def data_processing(data: list[dict]) -> pd.DataFrame:
+def data_processing(data:list[dict]) -> pd.DataFrame:
     """
-    Process data, convert data type, minor cleaning and keeping necessary columns.
-
-    Parameters:
-    data: extracted data.
-
-    Returns:
-    df: DataFrame contain processed data.
+    Process data, convert data types, minor cleaning, and keep necessary columns.
     """
-
+    logger = get_run_logger()
     df = pd.DataFrame(data)
 
+    # Normalize AQILast with prefix to avoid column name conflict
     expanded_aqi = pd.json_normalize(df['AQILast'])
     df = pd.concat([df, expanded_aqi], axis=1)
-
-    #convert data types
-    numeric_cols = ['PM25.color_id', 'PM25.value']
-
-    df[numeric_cols] = df[numeric_cols].astype(float)
-    df['time'] = df[df['date'] == df['date'].max()]['time'].max()
-    df['date'] = df['date'].max()
+    df['time'] = df['time'].mode()[0]
+    df['date'] = df['date'].mode()[0]
     df['timestamp'] = pd.to_datetime(df['date'] + ' ' + df['time'])
 
-    #make datetime attribute columns for partitioning in parquet file.
-    df['year'] = df['timestamp'].dt.year
-    df['month'] = df['timestamp'].dt.month
-    df['day'] = df['timestamp'].dt.day
-    df['hour'] = df['timestamp'].dt.hour
+    # Extract datetime components
+    df[['year', 'month', 'day', 'hour']] = df['timestamp'].apply(
+        lambda ts: pd.Series([ts.year, ts.month, ts.day, ts.hour])
+    )
 
-    #keep necessary columns
-    datetime_attribute_cols = ['timestamp', 'year', 'month', 'day', 'hour']
-    location_attribute_cols = ['stationID', 'nameTH', 'nameEN', 'areaTH', 'areaEN', 'stationType', 'lat', 'long']
-    df = df[datetime_attribute_cols + location_attribute_cols + numeric_cols]
+    # Convert numeric columns
+    df[['PM25.color_id', 'PM25.aqi']] = df[['PM25.color_id', 'PM25.aqi']].astype(int)
 
+    # Select columns
+    df = df[[
+        'timestamp', 'year', 'month', 'day', 'hour',
+        'stationID', 'nameTH', 'nameEN', 'areaTH', 'areaEN',
+        'stationType', 'lat', 'long', 'PM25.color_id', 'PM25.aqi'
+        ]]
+    
+    logger.info(f"Data processed, resulting DataFrame shape: {df.shape}")
     return df
 
 
@@ -80,49 +68,51 @@ def data_processing(data: list[dict]) -> pd.DataFrame:
 def load_to_lakefs(df: pd.DataFrame, lakefs_s3_path: str, storage_options: dict):
     """
     Load data into lakeFS storage in S3 (Amazon Simple Storage Service) and in a parquet format.
-    Partitions the parquet file base on 'year', 'month', 'day' and 'hour'.
+    Partitions the parquet file based on 'year', 'month', 'day' and 'hour'.
 
-    Parameters:
+    Parameters: 
     df: Target DataFrame that will be loaded into the storage.
     lakefs_s3_path: S3-Supported API Gateway, layer in lakeFS responsible for the compatibility with S3.
-    storage_options: configured option for accessing lakeFS storage.
+    storage_options: configured options for accessing lakeFS storage.
 
     Returns:
     None
     """
-    df.to_parquet(
-        lakefs_s3_path,
-        storage_options=storage_options,
-        partition_cols=['year', 'month', 'day', 'hour'],
-    )
-
+    logger = get_run_logger()
+    
+    try:
+        df.to_parquet(
+            lakefs_s3_path,
+            storage_options=storage_options,
+            partition_cols=['year', 'month', 'day', 'hour'],
+        )
+        logger.info(f"✅ Data successfully loaded to: {lakefs_s3_path}")
+    except Exception as e:
+        logger.error(f"Error loading data to lakeFS: {e}")
 
 @flow(name='main-flow', log_prints=True)
-def main_flow():
+def main_flow(
+    base_url: str="http://air4thai.pcd.go.th",
+    path: str="/services/getNewAQI_JSON.php"
+):
+    # Task 1: Fetch data from the API
+    data = retrieve_from_api(
+        base_url,
+        path
+    )
 
-    #Task 1 :Get data
-    data = get_aqi_data()
-
-    #Task 2: Process data
+    # Task 2: Data Process
     df = data_processing(data)
 
-    #lakeFS storage
-    #lakeFS credentials from your docker-compose.yml
+    # lakeFS setup
     ACCESS_KEY = "access_key"
     SECRET_KEY = "secret_key"
-
-    #lakeFS endpoint (running locally)
     lakefs_endpoint = "http://lakefs-dev:8000/"
-
-    #lakeFS repository, branch, and file path
     repo = "air-quality"
     branch = "main"
-    path = "airquality.parquet"
+    file_path = "airquality.parquet"
+    lakefs_s3_path = f"s3a://{repo}/{branch}/{file_path}"
 
-    #Construct the full lakeFS S3-compatible path
-    lakefs_s3_path = f"s3a://dsi321-record-{repo}/{branch}/{path}"
-
-    #configure storage_options for lakeFS (S3-compatible)
     storage_options = {
         "key": ACCESS_KEY,
         "secret": SECRET_KEY,
@@ -132,6 +122,4 @@ def main_flow():
     }
 
     # Task 3: Load data into lakeFS storage in a parquet format
-
-    load_to_lakefs(df=df, lakefs_s3_path=lakefs_s3_path, storage_options=storage_options)
-    
+    load_to_lakefs(df, lakefs_s3_path, storage_options)
